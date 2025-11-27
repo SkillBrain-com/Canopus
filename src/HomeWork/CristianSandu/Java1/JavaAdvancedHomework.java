c2376b6a95e910f56e5d35b984a50d32faa8fa82
Sandu Cristian Claudiu <sandu_cristian2007@yahoo.com>
2025-11-27 20:27:02 +0200

    my second homework

---
+++ src/HomeWork/CristianSandu/Java1/JavaAdvancedHomework.java
@@ -0,0 +1,8 @@
+package HomeWork.CristianSandu.Java1;
+
+public class JavaAdvancedHomework {
+
+    public static void main(String[] args) {
+        System.out.println("super complicated homework.");
+    }
+}
