6d7cd86e1a25749ee2855a57647e623f21723885
Sandu Cristian Claudiu <sandu_cristian2007@yahoo.com>
2025-11-27 20:22:06 +0200

    my first homework

---
+++ src/HomeWork/CristianSandu/Java1/JavaBasicsHomework.java
@@ -0,0 +1,8 @@
+package HomeWork.CristianSandu.Java1;
+
+public class JavaBasicsHomework {
+
+    public static void main(String[] args) {
+        System.out.println("cursul asta e de groaza!!!!");
+    }
+}
